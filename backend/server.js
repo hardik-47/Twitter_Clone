e9ddd8ec0db42bc8b8bcd3d703d01be00cf8539d
hardik-47 <jainhardik929@gmail.com>
2024-09-07 17:58:40 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import authRoutes from './routes/auth.routes.js'
 import userRoutes from './routes/user.routes.js'
 import postRoutes from './routes/post.routes.js'
 import notificationRoutes from './routes/notification.routes.js'
-import connectMongoDB from "./db/connectMongoDb.js";
+import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 // const cors =require('cors'); 
