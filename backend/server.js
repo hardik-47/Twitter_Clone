620fb207612e8caa1bb60b2085f7683a17566023
hardik-47 <jainhardik929@gmail.com>
2024-09-07 17:20:26 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 // import bcrypt from 'bcryptjs'
 import authRoutes from './routes/auth.routes.js'
 import userRoutes from './routes/user.routes.js'
@@ -29,7 +30,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = 5000;
-
+const __dirname=path.resolve();
 app.use(express.json({limit:"5mb"}));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -39,6 +40,16 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    // Serve static files from the "build" directory
+    app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+    // Handle SPA routing, serve index.html for all unknown routes
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`server is running at port ${PORT}`);
